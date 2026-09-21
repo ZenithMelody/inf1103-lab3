ba0206278a4a74d18d33415995b02c2c21645a8d
2602020 <2602020@sit.singaporetech.edu.sg>
2026-09-21 14:47:50 +0800

    added tax feature

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,5 +1,6 @@
 inventory = 0
 failed_Enteries = 0
+total_Tax = 0.0
 
 # handles prompt, input validation / return valid ints or 'quit' signal
 def get_valid_input():
@@ -20,10 +21,14 @@ def get_valid_input():
 def process_delivery(current_total, new_value):
         return current_total + new_value
 
+def calculate_tax(amount):
+        return amount * 0.10
+
 # report summary generation
-def generate_report(inventory, failed_Enteries):
+def generate_report(inventory, failed_Enteries, total_Tax):
         print("\nReport Summary")
         print(f"Total Units Processed: {inventory}")
+        print(f"Total amount of Tax: {total_Tax:.2f}")
         print(f"Number of Failed/Rejected Entries: {failed_Enteries}")
         
 while True:
@@ -33,15 +38,15 @@ while True:
     if user_Input == "quit":
             break
 
-    amount = int(user_Input)
-
+    tax = calculate_tax(user_Input)
+    total_Tax += tax
     inventory = process_delivery(inventory, user_Input)
 
     # state management
-    print(f"Added {amount} | Current Inventory: {inventory}")
+    print(f"Added {user_Input} | Tax: {tax:.2f} | Current Inventory: {inventory}")
 
     if inventory > 500:
         print("Over limit of 500")
         break
 
-generate_report(inventory, failed_Enteries)
+generate_report(inventory, failed_Enteries, total_Tax)
